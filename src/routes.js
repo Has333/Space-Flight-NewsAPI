78608819b78e1480c7cbe53cb5be820773ab3ef7
William Martins <williammds00@gmail.com>
2022-02-11 21:12:12 -0300

    feat: add all GET routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
-import express from 'express';
+import express from "express";
+import fetch from "node-fetch";
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -6,10 +8,34 @@ router.get("/", (req, res) => {
   res.send("Back-end Challenge 2021 🏅 - Space Flight News");
 });
 
-router.get("/articles", (req, res) => {
 
+router.get("/articles", async (req, res) => {
+  try {
+    const response = await fetch('https://api.spaceflightnewsapi.net/v3/articles');
+    const data = await response.json();
+    res.send(data)
+    res.status(200)
+  } 
+catch (err) {
+    console.log(err)
+    res.send('Unable to fetch data from API')
+    res.status(500)
+  }
 });
 
-router.get("/articles/{id}", (req, res) => {});
+
+router.get("/articles/:id", async (req, res) => {
+try {
+  const response = await fetch('https://api.spaceflightnewsapi.net/v3/articles/?_id=' + req.params.id);
+  const data = await response.json();
+  res.send(data)
+  res.status(200)
+} 
+catch (err) {
+  console.log(err)
+  res.send('Unable to fetch data from API')
+  res.status(500)
+}
+});
 
 export default router;
