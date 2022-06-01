739c405ba954cb7872ebbdc5b407e7d64efd6478
William Martins <williammds00@gmail.com>
2022-06-01 16:01:25 -0300

    refactor: organize db config

--- src/automation/upsert-articles-to-db.js
+++ src/automation/upsert-articles-to-db.js
@@ -1,4 +1,5 @@
 import { SpaceflightnewsArticles } from "../services/spaceflightnews.service.js";
+import { articleConfigModel } from "../common/helpers/articleConfig.helper.js";
 import { Article } from "../models/article.js";
 
 class AutomationFlow {
@@ -10,12 +11,9 @@ class AutomationFlow {
         for (currentArticle; currentArticle <= articlesAmount; currentArticle++) { 
              const getArticle = await SpaceflightnewsArticles.listArticles(currentArticle, articlesPerRequest);
              const { id, featured, title, url, imageUrl, newsSite, summary, publishedAt, provider } = getArticle[0];
+             const articleConfig = articleConfigModel(id, featured, title, url, imageUrl, newsSite, summary, publishedAt, provider); 
 
-             const filter = { id: id };
-             const update = { id: id, featured: featured, title: title, url: url, imageUrl: imageUrl, newsSite: newsSite, summary: summary, publishedAt: publishedAt, launches: [{ id: id, provider: provider }], events: [{ id: id, provider: provider }]};
-             const options = { new: true, upsert: true };
-
-             let newArticle = await Article.findOneAndUpdate(filter, update, options);
+             let newArticle = await Article.findOneAndUpdate(articleConfig.filter, articleConfig.update, articleConfig.options);
              
              if (currentArticle >= articlesAmount) {
                 console.log(`${newArticle.title} \u001b[1;32m${currentArticle}\u001b[0m/\u001b[1;32m${articlesAmount}\u001b[0m`); 
---
+++ src/common/helpers/articleConfig.helper.js
@@ -0,0 +1,9 @@
+function articleConfigModel(id, featured, title, url, imageUrl, newsSite, summary, publishedAt, provider){
+    const filter = { id: id };
+    const update = { id: id, featured: featured, title: title, url: url, imageUrl: imageUrl, newsSite: newsSite, summary: summary, publishedAt: publishedAt, launches: [{ id: id, provider: provider }], events: [{ id: id, provider: provider }]};
+    const options = { new: true, upsert: true };
+
+    return { filter, update, options };
+}
+
+export { articleConfigModel }
